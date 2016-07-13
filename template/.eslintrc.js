4996bc94b7862a3819c86a9f8b68e19bc32d7d67
Brad Stewart <bradstewart@users.noreply.github.com>
2016-07-13 16:04:11 -0500

    Add comma-dangle eslint rule

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -24,6 +24,8 @@ module.exports = {
     'import/no-unresolved': 0,
     {{/if_eq}}
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    // because I like it
+    'comma-dangle': [1, 'always-multiline'],
   }
 }
